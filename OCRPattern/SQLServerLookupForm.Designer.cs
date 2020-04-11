b9e1972b899592b6b3c156e084d39bd3d8e8fbab
kenjiuno <ku@digitaldolphins.jp>
2020-04-11 09:33:12 +0900

    SQLServerLookupForm 整備

--- OCRPattern/SQLServerLookupForm.Designer.cs
+++ OCRPattern/SQLServerLookupForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace OCRPattern
+namespace OCRPattern
 {
     partial class SQLServerLookupForm
     {
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.blkBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dcr = new OCRPattern.DCR();
             this.label1 = new System.Windows.Forms.Label();
             this.sqlConnStrText = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -39,14 +41,15 @@
             this.runBtn = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.sqlOutputColumnsText = new System.Windows.Forms.TextBox();
-            this.buildBtn = new System.Windows.Forms.Button();
-            this.testBtn = new System.Windows.Forms.Button();
-            this.buildQueryBtn = new System.Windows.Forms.Button();
-            this.blkBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dcr = new OCRPattern.DCR();
-            this.schemaMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.connStrMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.runConnTestMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.connStrBtn = new System.Windows.Forms.Button();
+            this.sqlQueryMenuBtn = new System.Windows.Forms.Button();
+            this.sqlQueryMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sqlOutputColumnMenu = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.blkBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dcr)).BeginInit();
+            this.connStrMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -59,6 +62,16 @@
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "SQL Server Lookup を使用します";
             this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // blkBindingSource
+            // 
+            this.blkBindingSource.DataMember = "Blk";
+            this.blkBindingSource.DataSource = this.dcr;
+            // 
+            // dcr
+            // 
+            this.dcr.DataSetName = "DCR";
+            this.dcr.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // label1
             // 
@@ -80,10 +93,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 148);
+            this.label2.Location = new System.Drawing.Point(12, 153);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 12);
-            this.label2.TabIndex = 5;
+            this.label2.TabIndex = 4;
             this.label2.Text = "SQL 問合せ文";
             // 
             // sqlQueryText
@@ -92,37 +105,37 @@
             this.sqlQueryText.AcceptsTab = true;
             this.sqlQueryText.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.blkBindingSource, "SQLQuery", true));
             this.sqlQueryText.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.sqlQueryText.Location = new System.Drawing.Point(12, 163);
+            this.sqlQueryText.Location = new System.Drawing.Point(12, 168);
             this.sqlQueryText.Multiline = true;
             this.sqlQueryText.Name = "sqlQueryText";
             this.sqlQueryText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.sqlQueryText.Size = new System.Drawing.Size(317, 133);
-            this.sqlQueryText.TabIndex = 6;
+            this.sqlQueryText.TabIndex = 5;
             this.sqlQueryText.WordWrap = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 366);
+            this.label3.Location = new System.Drawing.Point(12, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 12);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "@input サンプル文字列";
+            this.label3.Size = new System.Drawing.Size(199, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "@input パラメータに与えるサンプル文字列";
             // 
             // sqlInputSampleText
             // 
             this.sqlInputSampleText.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.blkBindingSource, "SQLInputSample", true));
-            this.sqlInputSampleText.Location = new System.Drawing.Point(12, 381);
+            this.sqlInputSampleText.Location = new System.Drawing.Point(12, 111);
             this.sqlInputSampleText.Name = "sqlInputSampleText";
             this.sqlInputSampleText.Size = new System.Drawing.Size(317, 19);
-            this.sqlInputSampleText.TabIndex = 9;
+            this.sqlInputSampleText.TabIndex = 10;
             // 
             // runBtn
             // 
-            this.runBtn.Location = new System.Drawing.Point(12, 406);
+            this.runBtn.Location = new System.Drawing.Point(12, 447);
             this.runBtn.Name = "runBtn";
             this.runBtn.Size = new System.Drawing.Size(75, 46);
-            this.runBtn.TabIndex = 10;
+            this.runBtn.TabIndex = 11;
             this.runBtn.Text = "テスト問合せ";
             this.runBtn.UseVisualStyleBackColor = true;
             this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
@@ -130,78 +143,81 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(351, 148);
+            this.label4.Location = new System.Drawing.Point(12, 315);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(118, 12);
-            this.label4.TabIndex = 11;
+            this.label4.TabIndex = 7;
             this.label4.Text = "出力のうち、採用する列";
             // 
             // sqlOutputColumnsText
             // 
             this.sqlOutputColumnsText.AcceptsReturn = true;
             this.sqlOutputColumnsText.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.blkBindingSource, "SQLOutputColumns", true));
-            this.sqlOutputColumnsText.Location = new System.Drawing.Point(353, 163);
+            this.sqlOutputColumnsText.Location = new System.Drawing.Point(12, 330);
             this.sqlOutputColumnsText.Multiline = true;
             this.sqlOutputColumnsText.Name = "sqlOutputColumnsText";
             this.sqlOutputColumnsText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.sqlOutputColumnsText.Size = new System.Drawing.Size(182, 133);
-            this.sqlOutputColumnsText.TabIndex = 12;
+            this.sqlOutputColumnsText.Size = new System.Drawing.Size(317, 94);
+            this.sqlOutputColumnsText.TabIndex = 8;
             this.sqlOutputColumnsText.WordWrap = false;
             // 
-            // buildBtn
+            // connStrMenu
             // 
-            this.buildBtn.Location = new System.Drawing.Point(12, 90);
-            this.buildBtn.Name = "buildBtn";
-            this.buildBtn.Size = new System.Drawing.Size(75, 46);
-            this.buildBtn.TabIndex = 3;
-            this.buildBtn.Text = "ビルド";
-            this.buildBtn.UseVisualStyleBackColor = true;
-            this.buildBtn.Click += new System.EventHandler(this.buildBtn_Click);
+            this.connStrMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runConnTestMenu});
+            this.connStrMenu.Name = "connStrMenu";
+            this.connStrMenu.Size = new System.Drawing.Size(167, 26);
             // 
-            // testBtn
+            // runConnTestMenu
             // 
-            this.testBtn.Location = new System.Drawing.Point(94, 90);
-            this.testBtn.Name = "testBtn";
-            this.testBtn.Size = new System.Drawing.Size(75, 46);
-            this.testBtn.TabIndex = 4;
-            this.testBtn.Text = "テスト";
-            this.testBtn.UseVisualStyleBackColor = true;
-            this.testBtn.Click += new System.EventHandler(this.testBtn_Click);
+            this.runConnTestMenu.Name = "runConnTestMenu";
+            this.runConnTestMenu.Size = new System.Drawing.Size(166, 22);
+            this.runConnTestMenu.Text = "接続テストを実行...";
+            this.runConnTestMenu.Click += new System.EventHandler(this.runConnTestMenu_Click);
             // 
-            // buildQueryBtn
+            // connStrBtn
             // 
-            this.buildQueryBtn.Location = new System.Drawing.Point(12, 302);
-            this.buildQueryBtn.Name = "buildQueryBtn";
-            this.buildQueryBtn.Size = new System.Drawing.Size(75, 46);
-            this.buildQueryBtn.TabIndex = 7;
-            this.buildQueryBtn.Text = "ビルド";
-            this.buildQueryBtn.UseVisualStyleBackColor = true;
-            this.buildQueryBtn.Click += new System.EventHandler(this.buildQueryBtn_Click);
+            this.connStrBtn.Location = new System.Drawing.Point(335, 65);
+            this.connStrBtn.Name = "connStrBtn";
+            this.connStrBtn.Size = new System.Drawing.Size(75, 19);
+            this.connStrBtn.TabIndex = 3;
+            this.connStrBtn.Text = "...";
+            this.connStrBtn.UseVisualStyleBackColor = true;
+            this.connStrBtn.Click += new System.EventHandler(this.connStrBtn_Click);
             // 
-            // blkBindingSource
+            // sqlQueryMenuBtn
             // 
-            this.blkBindingSource.DataMember = "Blk";
-            this.blkBindingSource.DataSource = this.dcr;
+            this.sqlQueryMenuBtn.Location = new System.Drawing.Point(335, 168);
+            this.sqlQueryMenuBtn.Name = "sqlQueryMenuBtn";
+            this.sqlQueryMenuBtn.Size = new System.Drawing.Size(75, 19);
+            this.sqlQueryMenuBtn.TabIndex = 6;
+            this.sqlQueryMenuBtn.Text = "...";
+            this.sqlQueryMenuBtn.UseVisualStyleBackColor = true;
+            this.sqlQueryMenuBtn.Click += new System.EventHandler(this.sqlQueryMenuBtn_Click);
             // 
-            // dcr
+            // sqlQueryMenu
             // 
-            this.dcr.DataSetName = "DCR";
-            this.dcr.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            this.sqlQueryMenu.Name = "sqlQueryMenu";
+            this.sqlQueryMenu.Size = new System.Drawing.Size(61, 4);
             // 
-            // schemaMenu
+            // sqlOutputColumnMenu
             // 
-            this.schemaMenu.Name = "schemaMenu";
-            this.schemaMenu.Size = new System.Drawing.Size(61, 4);
-            this.schemaMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.schemaMenu_ItemClicked);
+            this.sqlOutputColumnMenu.Location = new System.Drawing.Point(335, 330);
+            this.sqlOutputColumnMenu.Name = "sqlOutputColumnMenu";
+            this.sqlOutputColumnMenu.Size = new System.Drawing.Size(75, 19);
+            this.sqlOutputColumnMenu.TabIndex = 18;
+            this.sqlOutputColumnMenu.Text = "...";
+            this.sqlOutputColumnMenu.UseVisualStyleBackColor = true;
+            this.sqlOutputColumnMenu.Click += new System.EventHandler(this.sqlOutputColumnMenu_Click);
             // 
             // SQLServerLookupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 491);
-            this.Controls.Add(this.buildQueryBtn);
-            this.Controls.Add(this.testBtn);
-            this.Controls.Add(this.buildBtn);
+            this.ClientSize = new System.Drawing.Size(462, 524);
+            this.Controls.Add(this.sqlOutputColumnMenu);
+            this.Controls.Add(this.sqlQueryMenuBtn);
+            this.Controls.Add(this.connStrBtn);
             this.Controls.Add(this.sqlOutputColumnsText);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.runBtn);
@@ -219,6 +235,7 @@
             this.Load += new System.EventHandler(this.SQLServerLookupForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.blkBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dcr)).EndInit();
+            this.connStrMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -238,9 +255,11 @@
         private System.Windows.Forms.Button runBtn;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox sqlOutputColumnsText;
-        private System.Windows.Forms.Button buildBtn;
-        private System.Windows.Forms.Button testBtn;
-        private System.Windows.Forms.Button buildQueryBtn;
-        private System.Windows.Forms.ContextMenuStrip schemaMenu;
+        private System.Windows.Forms.ContextMenuStrip connStrMenu;
+        private System.Windows.Forms.ToolStripMenuItem runConnTestMenu;
+        private System.Windows.Forms.Button connStrBtn;
+        private System.Windows.Forms.Button sqlQueryMenuBtn;
+        private System.Windows.Forms.ContextMenuStrip sqlQueryMenu;
+        private System.Windows.Forms.Button sqlOutputColumnMenu;
     }
 }
--- OCRPattern/SQLServerLookupForm.cs
+++ OCRPattern/SQLServerLookupForm.cs
@@ -19,7 +19,65 @@ namespace OCRPattern
             this.blkBindingSource.DataSource = blkBindingSource;
         }
 
-        private void testBtn_Click(object sender, EventArgs e)
+        private void runBtn_Click(object sender, EventArgs e)
+        {
+            using (var db = new SqlConnection(sqlConnStrText.Text))
+            {
+                db.Open();
+
+                var command = new SqlCommand(sqlQueryText.Text, db);
+                command.Parameters.AddWithValue("@input", sqlInputSampleText.Text);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    var dataTable = new DataTable();
+                    dataTable.Load(reader);
+
+                    reader.Close();
+                    db.Close();
+
+                    var form = new Form()
+                    {
+                        Text = "SQL Server 問合せ結果",
+                        WindowState = FormWindowState.Maximized,
+                    };
+                    var grid = new DataGridView()
+                    {
+                        Parent = form,
+                        Dock = DockStyle.Fill,
+                        DataSource = dataTable,
+                        AllowUserToAddRows = false,
+                        AllowUserToDeleteRows = false,
+                    };
+                    form.Show();
+                }
+            }
+        }
+
+        private void ConfirmApply(TextBox textBox, string newText)
+        {
+            if (MessageBox.Show(this, newText, null, MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) == DialogResult.OK)
+            {
+                textBox.Text = newText;
+            }
+        }
+
+        private void SQLServerLookupForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SQLServerLookupForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            blkBindingSource.EndEdit();
+        }
+
+        private void connStrBtn_Click(object sender, EventArgs e)
+        {
+            connStrMenu.Show((Control)sender, Point.Empty);
+        }
+
+        private void runConnTestMenu_Click(object sender, EventArgs e)
         {
             try
             {
@@ -35,61 +93,27 @@ namespace OCRPattern
             }
         }
 
-        private void runBtn_Click(object sender, EventArgs e)
-        {
-            using (var db = new SqlConnection(sqlConnStrText.Text))
-            {
-                db.Open();
-
-                var command = new SqlCommand(sqlQueryText.Text, db);
-                command.Parameters.AddWithValue("@input", sqlInputSampleText.Text);
-
-                using (var reader = command.ExecuteReader())
-                {
-                    sqlOutputColumnsText.Text = string.Join("\r\n",
-                        Enumerable.Range(0, reader.FieldCount)
-                            .Select(i => reader.GetName(i))
-                    );
-                }
-            }
-        }
-
-        private void buildBtn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void SQLServerLookupForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void SQLServerLookupForm_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            blkBindingSource.EndEdit();
-        }
-
-        private void buildQueryBtn_Click(object sender, EventArgs e)
+        private void sqlQueryMenuBtn_Click(object sender, EventArgs e)
         {
             using (var db = new SqlConnection(sqlConnStrText.Text))
             {
                 db.Open();
                 var tables = db.GetSchema("Tables");
 
-                schemaMenu.Items.Clear();
+                sqlQueryMenu.Items.Clear();
                 foreach (var row in tables.Select())
                 {
-                    var item = schemaMenu.Items.Add($"{row[1]}/{row[2]}");
+                    var item = sqlQueryMenu.Items.Add($"{row[1]}/{row[2]}", null, onClick);
                     item.Tag = row.ItemArray.Cast<string>().Take(3).ToArray();
                 }
             }
 
-            schemaMenu.Show((Control)sender, Point.Empty);
+            sqlQueryMenu.Show((Control)sender, Point.Empty);
         }
 
-        private void schemaMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        private void onClick(object sender, EventArgs e)
         {
-            var restriction = (string[])e.ClickedItem.Tag;
+            var restriction = (string[])((ToolStripMenuItem)sender).Tag;
 
             using (var db = new SqlConnection(sqlConnStrText.Text))
             {
@@ -102,7 +126,38 @@ namespace OCRPattern
                     names.Add(row[3] + "");
                 }
 
-                sqlQueryText.Text = $"SELECT TOP 1\r\n {string.Join(",\r\n ", names.Select(name => $"[{name}]"))}\r\n\r\n FROM [{restriction[1]}].[{restriction[2]}]\r\n";
+                db.Close();
+
+                ConfirmApply(sqlQueryText, $"SELECT TOP 1\r\n {string.Join(",\r\n ", names.Select(name => $"[{name}]"))}\r\n\r\n FROM [{restriction[1]}].[{restriction[2]}]\r\n");
+            }
+        }
+
+        private void sqlOutputColumnMenu_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "SQL Server へ問合せを実行し、列名一覧を取得します。", null, MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) != DialogResult.OK)
+            {
+                return;
+            }
+
+            using (var db = new SqlConnection(sqlConnStrText.Text))
+            {
+                db.Open();
+
+                var command = new SqlCommand(sqlQueryText.Text, db);
+                command.Parameters.AddWithValue("@input", sqlInputSampleText.Text);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    var text = string.Join("\r\n",
+                        Enumerable.Range(0, reader.FieldCount)
+                            .Select(i => reader.GetName(i))
+                    );
+
+                    reader.Close();
+                    db.Close();
+
+                    ConfirmApply(sqlOutputColumnsText, text);
+                }
             }
         }
     }
