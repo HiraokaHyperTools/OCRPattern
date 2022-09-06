6020810320189955ba71eefa70d506c43e823829
kenjiuno <ku@digitaldolphins.jp>
2022-09-06 11:10:05 +0900

    0.24

--- OCRPattern/Properties/AssemblyInfo.cs
+++ OCRPattern/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyFileVersion("0.23")] // here
+[assembly: AssemblyFileVersion("0.24")] // here
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。 
 // アセンブリに関連付けられている情報を変更するには、
